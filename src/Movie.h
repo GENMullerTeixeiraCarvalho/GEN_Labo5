839847416ed787ece07e49b257db095a494c973f
Robin Müller <33422394+AziXus@users.noreply.github.com>
2020-06-03 16:27:13 +0200

    Refactor: Removed unused attributes. Class: Movie - Dead code

--- src/Movie.h
+++ src/Movie.h
@@ -8,10 +8,6 @@
 
 class Movie {
 public:
-    static const int CHILDRENS   = 2;
-    static const int REGULAR     = 0;
-    static const int NEW_RELEASE = 1;
-
     Movie( const std::string& title, std::shared_ptr<MoviePriceCode> priceCode = RegularPriceCode::getPriceCode() );
 
     std::shared_ptr<MoviePriceCode> getPriceCode() const;
